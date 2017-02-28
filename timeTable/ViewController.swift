5e405307a5a5f969a55470f97b0dd8186d2ba00f
Thanut sajjakulnukit <oat_thanut@hotmail.com>
2017-02-28 07:15:47 +0700

    Initial Commit

---
+++ timeTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  timeTable
+//
+//  Created by Thanut's iMac on 2/28/2560 BE.
+//  Copyright © 2560 oat_Thanut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
