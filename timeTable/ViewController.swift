214cff68971f1b4ea20e2fd5451f17469c1e8a73
Thanut sajjakulnukit <oat_thanut@hotmail.com>
2017-02-28 07:30:55 +0700

    initial commit

--- timeTable/ViewController.swift
+++ timeTable/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var sliderValue: UISlider!
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
